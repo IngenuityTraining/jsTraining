88d669c8b54d292e1c45dbebd7f6e62b77bd5c7f
ankush-badyal-23 <ankushbadyal@hotmail.com>
2021-07-01 09:56:20 +0530

    added form manipulation.

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-if(!confirm(' do you really want to enter the page')){
-    location.href = 'https://google.com';
+let count = 0;
+
+onload = function(){
+    document.getElementById("click").addEventListener("click", function(){
+        ++count;
+        console.log("count is ", count);
+        document.getElementById("rightTopDiv").innerHTML += "<br/>Count is now " + count;
+        document.getElementById("username").value = count;
+    });
+    document.getElementById("username").addEventListener("change", function(){
+        this.value = this.value.toUpperCase();
+    });
 }
